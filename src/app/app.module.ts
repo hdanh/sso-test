884d7191e07481df62847830053f8bc584aef711
Danh Hoang <danh.hoang@2359media.com>
2020-10-08 15:37:56 +0700

    edit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,26 +9,32 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared';
 import { HomeComponent } from './home';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
-
+import { TestComponent } from './test/test.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, TestComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     CoreModule.forRoot(),
-    RouterModule.forRoot([
-      { path: '', pathMatch: 'full', redirectTo: 'home' },
-      { path: 'home', component: HomeComponent, canActivate: [AuthGuard] }
-    ], { useHash: true, preloadingStrategy: PreloadAllModules }),
+    RouterModule.forRoot(
+      [
+        { path: '', pathMatch: 'full', redirectTo: 'home' },
+        { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+        { path: 'test', component: TestComponent },
+      ],
+      { useHash: true, preloadingStrategy: PreloadAllModules }
+    ),
     SharedModule,
-    AuthModule
+    AuthModule,
   ],
   providers: [
     {
-      provide: HTTP_INTERCEPTORS, useClass: AppHttpInterceptor, multi: true
-    }
+      provide: HTTP_INTERCEPTORS,
+      useClass: AppHttpInterceptor,
+      multi: true,
+    },
   ],
   bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/test/test.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.pug',
+  styleUrls: ['./test.component.scss'],
+})
+export class TestComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
